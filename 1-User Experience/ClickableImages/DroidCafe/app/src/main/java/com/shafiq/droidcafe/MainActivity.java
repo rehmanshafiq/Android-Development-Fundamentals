0a797c196d6b0a94482d54d3fef74661519f88ad
shafiqrrehman <shafiq_1309e@aptechgdn.com>
2019-12-09 23:06:38 +0500

    Passed data to OrderActivity.

--- 1-User Experience/ClickableImages/DroidCafe/app/src/main/java/com/shafiq/droidcafe/MainActivity.java
+++ 1-User Experience/ClickableImages/DroidCafe/app/src/main/java/com/shafiq/droidcafe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.shafiq.droidcafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -16,29 +17,27 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String EXTRA_MESSAGE =
+            "com.shafiq.droidcafe.extra.MESSAGE";
+
     ImageView donut, froyo, icecream;
+
+    String mOrderMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         donut = findViewById(R.id.donut);
         donut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayToastMessage(getString(R.string.donut_order_message));
+                mOrderMessage = getString(R.string.donut_order_message);
+                displayToastMessage(mOrderMessage);
             }
         });
 
@@ -46,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
         froyo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayToastMessage(getString(R.string.froyo_order_message));
+                mOrderMessage = getString(R.string.froyo_order_message);
+                displayToastMessage(mOrderMessage);
             }
         });
 
@@ -54,7 +54,19 @@ public class MainActivity extends AppCompatActivity {
         icecream.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayToastMessage(getString(R.string.ice_cream_order_message));
+                mOrderMessage = getString(R.string.ice_cream_order_message);
+                displayToastMessage(mOrderMessage);
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
             }
         });
     }
